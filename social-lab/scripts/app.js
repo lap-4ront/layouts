4efd9b1ce71fa81a5a81cb905fbef28cce6cc42b
Rinat Valiullov <4rontender@gmail.com>
2022-04-08 12:32:26 +0500

    footer, modal, scrollspy

--- social-lab/scripts/app.js
+++ social-lab/scripts/app.js
@@ -1,2 +1,7 @@
 const YEAR = document.querySelector("#year");
 YEAR.innerHTML = new Date().getFullYear();
+
+// Init scrollspy
+let scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: "#main-nav"
+});
