7fa2afa40d8c86cd47958f85250db28a02884032
Rinat Valiullov <4rontender@gmail.com>
2022-04-18 15:34:36 +0500

    video-play section with video autoplay logic in the modal

--- glozzom-multipage/scripts/app.js
+++ glozzom-multipage/scripts/app.js
@@ -4,3 +4,27 @@ const myCarousel = document.querySelector("#myCarousel");
 const carousel = new bootstrap.Carousel(myCarousel, {
   interval: 7000
 });
+
+// Playing video in the modal
+const autoPlayYouTubeModal = () => {
+  const triggerOpen = document.querySelector("[data-video]");
+
+  triggerOpen.addEventListener("click", function () {
+    let theModal = document.querySelector(this.dataset.bsTarget);
+    let videoSRC = this.getAttribute("data-video");
+    let videoSRCauto =
+      videoSRC +
+      "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+
+    let theModalIframe = theModal.querySelector("iframe");
+    theModalIframe.setAttribute("src", videoSRCauto);
+
+    let theModalClose = theModal.querySelector("button.btn-close");
+
+    theModalClose.addEventListener("click", () => {
+      theModalIframe.setAttribute("src", videoSRC);
+    });
+  });
+};
+
+autoPlayYouTubeModal();
