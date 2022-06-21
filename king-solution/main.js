5609c6ec35312ef419a0ebc49c4d7301cc244061
Rinat Valiullov <4rontender@gmail.com>
2022-06-21 23:02:13 +0500

    contact-modal logic

--- king-solution/main.js
+++ king-solution/main.js
@@ -6,15 +6,23 @@ FORM.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
+const OVERLAY = document.querySelector(".overlay");
 const CONTACT_BTN = document.querySelector(".contact-btn");
+const GET_STARTED_BTN = document.querySelector(".header__cta-btn");
+const CONTACT_MODAL = document.querySelector(".contact-modal");
+
+GET_STARTED_BTN.addEventListener("click", (event) => {
+  CONTACT_MODAL.style.display = "block";
+  OVERLAY.style.display = "block";
+});
+
 document.addEventListener("click", (event) => {
-  // console.log(event.target.classList.contains("contact-btn"));
   const GETINTOUCH_MODAL = document.querySelector(".getintouch-modal");
-  const OVERLAY = document.querySelector(".overlay");
   if (event.target.classList.contains("contact-btn")) {
     OVERLAY.style.display = "block";
     GETINTOUCH_MODAL.style.display = "flex";
     GETINTOUCH_MODAL.classList.toggle("show");
+    CONTACT_MODAL.style.display = "none";
   }
 
   if (
